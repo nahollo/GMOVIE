312ffff8d115131ff208ed30c936444e92fd7943
NaHyeongJin <skgudwlsdlrj@gmail.com>
2023-09-22 16:56:42 +0900

    first project

---
+++ src/main/java/com/gmovie/gmovie/GmovieApplication.java
@@ -0,0 +1,13 @@
+package com.gmovie.gmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmovie/gmovie/GmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmovie.gmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
