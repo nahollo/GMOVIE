59f1b00c64b760c489335a0b2ad4045cdb89dbf5
NaHyeongJin <skgudwlsdlrj@gmail.com>
2023-09-23 15:19:58 +0900

    feat)_Na_add_papagoAPI
    파파고 API 추가함.
    컨트롤러에서 get 방식으로
    temp라는 메서드를 만듬.
    /temp에서 번역한 값 볼 수있음

--- src/main/java/com/gmovie/gmovie/GmovieApplication.java
+++ src/main/java/com/gmovie/gmovie/GmovieApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GmovieApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GmovieApplication.class, args);
 	}
-	// 나형진
-
 }
---
+++ src/main/java/com/gmovie/gmovie/controller/TranslationController.java
@@ -0,0 +1,53 @@
+package com.gmovie.gmovie.controller;
+
+import com.gmovie.gmovie.service.TranslationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+
+@Controller
+public class TranslationController {
+    private final TranslationService translationService;
+
+    @Autowired
+    public TranslationController(TranslationService translationService) {
+        this.translationService = translationService;
+    }
+
+    @GetMapping("/translateEngToKor")
+    public String translateEngToKor(@RequestParam String text) {
+        return translationService.engToKor(text);
+    }
+
+    @GetMapping("/translateKorToEng")
+    public String translateKorToEng(@RequestParam String text) {
+        return translationService.korToEng(text);
+    }
+
+    @PostMapping("/translate")
+    public String translate(@RequestBody String text, @RequestParam String sourceLang, @RequestParam String targetLang) {
+        // 필요한 로직을 구현하여 번역 요청 처리
+        // translationService.translate(text, sourceLang, targetLang);
+        return "번역 결과";
+    }
+
+    @GetMapping("/temp")
+    public String temp(Model model) {
+        String originalText = "apple";
+        String toEngText = translationService.korToEng(originalText);
+        String toKorText = translationService.engToKor(toEngText);
+    
+        // 모델에 데이터 추가
+        model.addAttribute("originalText", originalText);
+        model.addAttribute("toEngText", toEngText);
+        model.addAttribute("toKorText", toKorText);
+    
+        return "translationResult";
+    }
+
+}
