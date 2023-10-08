e5d24491ff936a4c2466fc62eaae1de3047427b6
gwanghuiLee <lgh970409@gmail.com>
2023-10-08 17:44:55 +0900

    feat)services_fix

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var logo = document.getElementById("gmovie-logo");
     var text = document.getElementById("gmovie-text");
+    var loginBtn = document.getElementById("login-btn"); // 로그인 버튼을 찾습니다.
 
     logo.addEventListener("click", function() {
         window.location.href = "home.html";
@@ -8,5 +9,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     text.addEventListener("click", function() {
         window.location.href = "home.html";
+    });
+
+    // 로그인 버튼을 클릭하면 signup.html로 이동하게 합니다.
+    loginBtn.addEventListener("click", function() {
+        window.location.href = "signup.html";
     });
 });
