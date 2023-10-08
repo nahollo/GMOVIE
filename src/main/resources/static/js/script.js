01bb6de8ea9816cecee6801adcbc1d0c41407c0c
gwanghuiLee <lgh970409@gmail.com>
2023-10-08 17:11:25 +0900

    feat)experts_js_finish

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var logo = document.getElementById("gmovie-logo");
+    var text = document.getElementById("gmovie-text");
+
+    logo.addEventListener("click", function() {
+        window.location.href = "home.html";
+    });
+
+    text.addEventListener("click", function() {
+        window.location.href = "home.html";
+    });
+});
